cad4e627b235744549f706f7a30b1a62d204ee58
Mitch Atkinson <mitchell.atkinson@outlook.com>
2018-12-27 21:20:40 +1000

    Added logic to determine next move

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,9 +6,9 @@ import './index.css';
     return (
       <button
         className="square"
-        onClick={() => this.props.onClick()}
+        onClick={() => props.onClick()}
       >
-        {this.props.value}
+        {props.value}
       </button>
     );
   }
@@ -18,13 +18,17 @@ import './index.css';
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       }
     }
 
     handleClick(i) {
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
 
     renderSquare(i) {
@@ -37,7 +41,7 @@ import './index.css';
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
