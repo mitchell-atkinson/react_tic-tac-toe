9a8451ec36e06f3c0f795ae0a32a7312d6a29790
Mitch Atkinson <mitchell.atkinson@outlook.com>
2018-12-29 11:49:01 +1000

    Finished time travel and completed tutorial

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -73,12 +73,13 @@ import './index.css';
         history: [{
           squares: Array(9).fill(null),
         }],
+        stepNumber: 0,
         xIsNext: true,
       }
     }
 
     handleClick(i) {
-      const history = this.state.history;
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
       const squares = current.squares.slice();
       if (calculateWinner(squares) || squares[i]) {
@@ -87,15 +88,23 @@ import './index.css';
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
         history: history.concat([{
-          squares: squares,
+          squares: squares
         }]),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
+      });
+    }
+
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
       });
     }
 
     render() {
       const history = this.state.history;
-      const current = history[history.length - 1];
+      const current = history[this.state.stepNumber]
       const winner = calculateWinner(current.squares);
 
       const moves = history.map((step, move) => {
@@ -103,7 +112,7 @@ import './index.css';
           'Go to move #' + move :
           'Go to game start';
         return (
-          <li>
+          <li key={move}>
             <button onClick={() => this.jumpTo(move)}>{desc}</button>
           </li>
         );
